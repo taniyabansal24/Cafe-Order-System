ba0307eab141501f8c8683130c8cb0dec2f11a28
Taniya Bansal <taniya1agarwal@gmail.com>
2025-12-06 11:54:26 +0530

    resolve

--- src/app/(auth)/verify-phone/page.js
+++ src/app/(auth)/verify-phone/page.js
@@ -1,5 +1,6 @@
 // src/app/(auth)/verify-phone/page.js
-import VerifyPhoneWrapper from "./VerifyPhoneWrapper";
+import VerifyPhoneWrapper from "./VerifyPhonePage.js";
+
 
 export default function Page() {
   return <VerifyPhoneWrapper />;
